6277a8e5f9b9baa01fdbeaff83562314619acbf7
saber1173 <46358399+saber1173@users.noreply.github.com>
2021-08-04 15:33:09 -0400

    working on training

--- src/train_salt.py
+++ src/train_salt.py
@@ -201,8 +201,8 @@ def main(_argv):
 # checkpoints/yolov3_train_{epoch}.tf
 
         callbacks = [
-            ReduceLROnPlateau(monitor="val_loss", factor=0.1, patience=3, verbose=1),
-            EarlyStopping(patience=10, verbose=1),
+            ReduceLROnPlateau(monitor="val_loss", factor=0.1, patience=1, verbose=1),
+            EarlyStopping(patience=5, verbose=1),
             ModelCheckpoint(FLAGS.output[:-3] + '_{epoch}.tf',
                             verbose=1, save_weights_only=True),
             TensorBoard(log_dir='logs')
